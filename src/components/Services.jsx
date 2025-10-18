27fe9670f72ba223da2cefa1a2068a7942ffb751
Ijan <ijanvdwestz@gmail.com>
2025-10-18 19:51:43 +0200

    service section update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import QuoteModal from "../Pages/QuoteModal"; // ✅ use modal instead of navigating
+import QuoteModal from "../Pages/QuoteModal";
 import "../styles/Services.css";
 
 const services = [
